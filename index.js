4dc6d03ab8911528c43dd80038c0d0779865c24e
RoUchiha <swaggsikh@gmail.com>
2022-02-21 01:05:16 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const timeparts = time.split(':') 
+  if (parseInt(timeparts[0]) < 12) {
+    return "Good Morning" 
+  }
+  if (parseInt(timeparts[0]) >= 12 && timeparts[0] < 17) {
+    return "Good Afternoon"
+  }
+  if (parseInt(timeparts[0]) >= 17) {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  const input = document.getElementById('greeting');
+  input.innerHTML = `${msg}`
+
+}
